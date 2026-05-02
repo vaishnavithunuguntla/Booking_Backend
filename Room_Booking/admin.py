ea376b54374c43056c541a709e17e56435607127
vaishnavi <suresh.thunuguntla78@gmail.com>
2026-05-02 16:00:49 +0530

    Custom User model, EmailBackend, and UserAdmin setup

--- Room_Booking/admin.py
+++ Room_Booking/admin.py
@@ -1,13 +1,42 @@
 
 
-from django.contrib import admin
-from .models import User
+# from django.contrib import admin
+# from .models import User
 
-from .models import Room,OccupiedDate,RoomImage
-# Register your models here.
-# Register your models here.
-# this willmake sure we will see it in admin panel
+# from .models import Room,OccupiedDate,RoomImage
+# # Register your models here.
+# # Register your models here.
+# # this willmake sure we will see it in admin panel
+# admin.site.register(Room)
+# admin.site.register(User)
+# admin.site.register(OccupiedDate)
+# admin.site.register(RoomImage)
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Room, RoomImage, OccupiedDate
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "full_name", "is_staff", "is_superuser")
+    search_fields = ("email", "full_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("full_name",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "full_name", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
+
 admin.site.register(Room)
-admin.site.register(User)
-admin.site.register(OccupiedDate)
 admin.site.register(RoomImage)
+admin.site.register(OccupiedDate)
--- Room_Booking/auth_backend.py
+++ Room_Booking/auth_backend.py
@@ -1,17 +1,21 @@
-from django.contrib.auth import get_user_model
-from typing import Any
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.http import HttpRequest
+from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, username = None, password = None, **kwargs):
-        UserModel=get_user_model()
+    """
+    Custom authentication backend that allows login with email instead of username.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        # Support both "username" and "email" keys
+        email = kwargs.get("email", username)
+        if email is None or password is None:
+            return None
         try:
-            user=UserModel.objects.get(email=username)
+            user = UserModel.objects.get(email__iexact=email)  # case-insensitive
         except UserModel.DoesNotExist:
             return None
-        else:
-            if user.check_password(password):
-                return user
-            return None
+        if user.check_password(password):
+            return user
+        return None
